e4afeaa1d64b9257f1e10422bce33a4affa97a3b
bardia-mhd <Bbardia2001>
2020-04-29 18:19:26 +0430

    Account's Structure (On master)

--- src/Model/Account.java
+++ src/Model/Account.java
@@ -1,4 +1,93 @@
 package Model;
 
+import java.util.ArrayList;
+
 public abstract class Account {
+    private ArrayList<Account> allAccounts =new ArrayList<Account>();
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String password;
+    private ArrayList<DiscountCode> discountList = new ArrayList<DiscountCode>();
+    private double credit;
+
+    public Account(String username, String firstName, String lastName, String email, String phoneNumber, String password, ArrayList<DiscountCode> discountList, double credit) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+        this.discountList = discountList;
+        this.credit = credit;
+        allAccounts.add(this);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public double getCredit() {
+        return credit;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void removeAccount (Account account){
+        allAccounts.remove(account);
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "username='" + username + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", password='" + password + '\'' +
+                ", discountList=" + discountList +
+                ", credit=" + credit +
+                '}';
+    }
 }
