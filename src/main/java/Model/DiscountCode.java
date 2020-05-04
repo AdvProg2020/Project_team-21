832ce5517a366cd17e6ba2837c78898b7ae95d02
SinaElahimanesh <59636948+SinaElahimanesh@users.noreply.github.com>
2020-05-05 00:02:44 +0430

    add methods and fields to Class DiscountCode

--- src/main/java/Model/DiscountCode.java
+++ src/main/java/Model/DiscountCode.java
@@ -1,4 +1,67 @@
 package Model;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class DiscountCode {
+
+    private static ArrayList<DiscountCode> allDiscountCodes = new ArrayList<>();
+    private String discountId;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private int discountPercentage;
+    private double maxDiscountAmount;
+    private int discountNumberForEachUser;
+    private HashMap<Account, Integer> discountUsers;
+
+    // Initialization Block
+    {
+        discountUsers = new HashMap<>();
+    }
+
+    public DiscountCode(String discountId, LocalDateTime startTime, LocalDateTime endTime, int discountPercentage, double maxDiscountAmount, int discountNumberForEachUser){
+        this.discountId = discountId;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.discountPercentage = discountPercentage;
+        this.maxDiscountAmount = maxDiscountAmount;
+        this.discountNumberForEachUser = discountNumberForEachUser;
+    }
+
+    public String getDiscountId() {
+        return discountId;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public int getDiscountPercentage() {
+        return discountPercentage;
+    }
+
+    public void setDiscountPercentage(int discountPercentage) {
+        this.discountPercentage = discountPercentage;
+    }
+
+    public double getMaxDiscountAmount() {
+        return maxDiscountAmount;
+    }
+
+    public void setMaxDiscountAmount(double maxDiscountAmount) {
+        this.maxDiscountAmount = maxDiscountAmount;
+    }
 }
