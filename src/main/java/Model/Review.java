94d5a0a07ba526f5db8ef80d43f88f1d19556125
SinaElahimanesh <59636948+SinaElahimanesh@users.noreply.github.com>
2020-05-05 12:38:04 +0430

    add mehtods and fields to Class Review

--- src/main/java/Model/Review.java
+++ src/main/java/Model/Review.java
@@ -1,4 +1,49 @@
 package Model;
 
 public class Review {
+
+    private Account user;
+    private Product product;
+    private String reviewText;
+    private boolean hasBought;
+
+    public Review(Account user, Product product, String reviewText, boolean hasBought){
+
+        setUser(user);
+        setProduct(product);
+        setReviewText(reviewText);
+        setHasBought(hasBought);
+    }
+
+    private void setUser(Account user) {
+        this.user = user;
+    }
+
+    private void setProduct(Product product) {
+        this.product = product;
+    }
+
+    private void setReviewText(String reviewText) {
+        this.reviewText = reviewText;
+    }
+
+    private void setHasBought(boolean hasBought) {
+        this.hasBought = hasBought;
+    }
+
+    public Account getUser() {
+        return user;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public String getReviewText() {
+        return reviewText;
+    }
+
+    public boolean hasBought(){
+        return hasBought;
+    }
 }
