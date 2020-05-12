68dfc1f96edf1b9b5164cac583e37560436490a8
bardia-mhd <Bbardia2001>
2020-05-12 11:23:28 +0430

    log and username in Control

--- src/main/java/Controller/Control.java
+++ src/main/java/Controller/Control.java
@@ -1,24 +1,49 @@
 package Controller;
 
-
+import Model.Manager;
+import Model.ShoppingCart;
+import View.ErrorsAndExceptions;
 import Model.Account;
 
+import javax.swing.text.View;
+
 public class Control {
-    Account user;
+    public static Account account;
+    public static ShoppingCart shoppingCart;
+
     private static Control instance;
     private Control()
     {
     }
 
+    public void usernameTypeValidation(String username, String type) throws ErrorsAndExceptions.ExistedUsernameException,
+            ErrorsAndExceptions.AdminRegisterException {
+        if (Account.getAccountByUsername(username) == null)
+            throw new ErrorsAndExceptions.ExistedUsernameException(username);
+        else if (type.equalsIgnoreCase("admin") && (Manager.getManager() == null))
+            throw new ErrorsAndExceptions.AdminRegisterException();
+    }
+
+    public void login(String username, String password) throws ErrorsAndExceptions.WrongPasswordException, ErrorsAndExceptions.InvalidUsernameException {
+        Account account = Account.getAccountByUsername(username);
+        if (account == null)
+            throw new ErrorsAndExceptions.InvalidUsernameException(username);
+        if (!account.getPassword().equals(password))
+            throw new ErrorsAndExceptions.WrongPasswordException();
+        this.account = account;
+    }
+
+
+
     public static Control getInstance() {
         return instance;
     }
 
-    public Account getUser() {
-        return user;
+    public Account getAccount() {
+        return account;
     }
 
-    public void setUser(Account user) {
-        this.user = user;
+    public void setAccount(Account account) {
+        this.account = account;
     }
 }
