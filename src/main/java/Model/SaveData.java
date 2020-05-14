a7d6fbb7f6f0377521bbf05314b6b4259962add1
SinaElahimanesh <59636948+SinaElahimanesh@users.noreply.github.com>
2020-05-14 14:59:23 +0430

    add file names ,where data should be saved, to Save Data Class

--- src/main/java/Model/SaveData.java
+++ src/main/java/Model/SaveData.java
@@ -11,7 +11,19 @@ public class SaveData {
     private static ArrayList<String> allFilesAdd = new ArrayList<>();
 
     {
-
+        allFilesAdd.add("Account.txt");
+        allFilesAdd.add("BuyLog.txt");
+        allFilesAdd.add("Category.txt");
+        allFilesAdd.add("Customer.txt");
+        allFilesAdd.add("DiscountCode.txt");
+        allFilesAdd.add("Manager.txt");
+        allFilesAdd.add("Off.txt");
+        allFilesAdd.add("Product.txt");
+        allFilesAdd.add("Review.txt");
+        allFilesAdd.add("Score.txt");
+        allFilesAdd.add("Seller.txt");
+        allFilesAdd.add("SellLog.txt");
+        allFilesAdd.add("ShoppingCart.txt");
     }
 
     public void readData(Object object){
