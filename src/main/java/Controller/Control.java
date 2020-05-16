76d652202aebdbf62a6c45d40e03120fa7cdfdfd
SinaElahimanesh <59636948+SinaElahimanesh@users.noreply.github.com>
2020-05-16 14:31:12 +0430

    fix methods for sorting allAccounts HashMap by alphabet usernames

--- src/main/java/Controller/Control.java
+++ src/main/java/Controller/Control.java
@@ -154,14 +154,13 @@ public class Control {
     }
 
 
-
     public Map<String, Account> sortAllAccounts() {
         Map<String, Account> allAccounts = Account.getAllAccounts();
         /* Sort statement */
         allAccounts = sortByKey(allAccounts);
         return allAccounts;
     }
-    
+
     public Map<String, Account> sortByKey(Map map) {
         // TreeMap to store values of HashMap
         TreeMap<String, Account> sorted = new TreeMap<>();
@@ -169,6 +168,7 @@ public class Control {
         // Copy all data from hashMap into TreeMap
         sorted.putAll(map);
 
-       return sorted;
+        return sorted;
     }
+    
 }
