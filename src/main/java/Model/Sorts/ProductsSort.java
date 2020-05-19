003afbebfcab776aea440a2cd2711418fb4b3e45
bardia-mhd <Bbardia2001>
2020-05-19 13:40:04 +0430

    fixing Names Problems

--- src/main/java/Model/Sorts/ProductsSort.java
+++ src/main/java/Model/Sorts/ProductsSort.java
@@ -7,33 +7,33 @@ import java.util.Comparator;
 public abstract class ProductsSort implements Comparator {
 
     public static class productSortByNameAscending implements Comparator<Product>{
-        public int compare(Product one , Product two){
-            return one.getName().compareToIgnoreCase(two.getName());
+        public int compare(Product firstOne , Product secondOne){
+            return firstOne.getName().compareToIgnoreCase(secondOne.getName());
         }
     }
 
     public static class productSortByNameDescending implements Comparator<Product> {
-        public int compare(Product one, Product two) {
-            return (-1) * (one.getName().compareToIgnoreCase(two.getName()));
+        public int compare(Product firstOne, Product secondOne) {
+            return (-1) * (firstOne.getName().compareToIgnoreCase(secondOne.getName()));
         }
     }
 
 
     public static class productSortByRate implements Comparator<Product> {
-        public int compare(Product one, Product two) {
-            return (-1) * (one.getBuyersAverageScore().compareTo(two.getBuyersAverageScore()));
+        public int compare(Product firstOne, Product secondOne) {
+            return (-1) * (firstOne.getBuyersAverageScore().compareTo(secondOne.getBuyersAverageScore()));
         }
     }
 
     public static class productSortByPriceAscending implements Comparator<Product> {
-        public int compare(Product one, Product two) {
-            return (one.getProductFinalPriceConsideringOff().compareTo(two.getProductFinalPriceConsideringOff()));
+        public int compare(Product firstOne, Product secondOne) {
+            return (firstOne.getProductFinalPriceConsideringOff().compareTo(secondOne.getProductFinalPriceConsideringOff()));
         }
     }
 
     public static class productSortByPriceDescendingly implements Comparator<Product> {
-        public int compare(Product one, Product two) {
-            return (-1) * (one.getProductFinalPriceConsideringOff().compareTo(two.getProductFinalPriceConsideringOff()));
+        public int compare(Product firstOne, Product secondOne) {
+            return (-1) * (firstOne.getProductFinalPriceConsideringOff().compareTo(secondOne.getProductFinalPriceConsideringOff()));
         }
     }
 }
